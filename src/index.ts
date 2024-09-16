f64b0d4e863dbee6b1e9ab0800250219df2890e5
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-09-16 01:23:03 +0100

    description and mrore

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import {type Result,ok,err} from "npm:neverthrow@8.0.0";
 
 type AnyFunction=(...args:any[])=>any;
 
-
+/**
+**/
 export function makeSafeFunction<F extends AnyFunction>(fn:F):(...args:Parameters<F>)=>Result<ReturnType<F> extends Promise<infer R>?R:ReturnType<F>,Error>{
 	// @ts-ignore: still yet to figure this out but it works
 	return (...args:Parameters<F>)=>{
