3de10e2c36760f31f6ffe1f8a56bb9d328225797
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-09-16 01:29:39 +0100

    Added JS Doc forinline documentation

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,15 @@ import {type Result,ok,err} from "npm:neverthrow@8.0.0";
 type AnyFunction=(...args:any[])=>any;
 
 /**
+* Wraps a function that could possibly throw
+* and returns a new function that uses Result<T,E>
+* to ensure errors are handled
+* 
+* @template F the type of the function to wrap
+* @param fn The Function being wrapped
+* @returns A new function that returns a Result Type 
+*
+*
 **/
 export function makeSafeFunction<F extends AnyFunction>(fn:F):(...args:Parameters<F>)=>Result<ReturnType<F> extends Promise<infer R>?R:ReturnType<F>,Error>{
 	// @ts-ignore: still yet to figure this out but it works
